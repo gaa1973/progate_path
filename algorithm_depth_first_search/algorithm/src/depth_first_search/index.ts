04e00d04d9dce8af97d10b8fc3d4f8a0622e4c6e
gaa <gaa197303@gmail.com>
2026-01-23 21:12:53 +0900

    ファイルツリー探索

--- algorithm_depth_first_search/algorithm/src/depth_first_search/index.ts
+++ algorithm_depth_first_search/algorithm/src/depth_first_search/index.ts
@@ -8,4 +8,19 @@ export const depthFirstSearch = (
   root: FileSystemNode,
   target: string,
 ): boolean => {
+  const stack: FileSystemNode[] = [root];
+
+  while (stack.length > 0) {
+    const current = stack.pop() as FileSystemNode;
+
+    if (!current.isDirectory && current.name === target) return true;
+
+    if (current.isDirectory && current.children?.length) {
+      for (let i = current.children.length - 1; i >= 0; i -= 1) {
+        stack.push(current.children[i]);
+      }
+    }
+  }
+
+  return false;
 };
