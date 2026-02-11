59c723b14ae8501c65889e5ded336ecc1fd9520d
gaa <gaa197303@gmail.com>
2026-02-11 20:19:37 +0900

    CSS

---
+++ static_webpage_devtool_elements/static_webpage/jest-puppeteer.config.js
@@ -0,0 +1,13 @@
+
+const port = process.env.PORT ?? 4446;
+export default {
+  server: {
+    command: `npm start -- -p ${port}`,
+    port: port,
+    launchTimeout: 30000,
+  },
+  launch: {
+    headless: "new",
+  },
+  browserContext: "incognito",
+};
